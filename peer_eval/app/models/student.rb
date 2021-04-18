4f0249ecdcfd03f55e35e69850edfbb78ec8d83f
Michael Rocca <mrocca1998@gmail.com>
2021-04-18 18:08:53 -0400

    readme and css touchups

--- peer_eval/app/models/student.rb
+++ peer_eval/app/models/student.rb
@@ -5,4 +5,6 @@ class Student < ApplicationRecord
   has_many :received_scores, class_name: "Score", foreign_key: "recipient_id"
   has_many :scores
   has_many :reviews
+  validates :fname, :lname, :email, presence: true
+  validates :groups, absence: { message: 'cannot have admins as members' }, if: :isAdmin?
 end
