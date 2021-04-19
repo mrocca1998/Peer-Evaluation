4f84b8898940216d62236ae0920f0699ec0f2725
noahz <noahzhang1@gmail.com>
2021-04-19 03:49:04 -0400

    Updated signup

--- peer_eval/app/controllers/application_controller.rb
+++ peer_eval/app/controllers/application_controller.rb
@@ -1,6 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_student!
 
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:fname, :lname, :email, :password)}
+
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:fname, :lname, :email, :password, :current_password)}
+  end
+
 
   def after_sign_in_path_for(resource)
     if current_student.try(:isAdmin?)
