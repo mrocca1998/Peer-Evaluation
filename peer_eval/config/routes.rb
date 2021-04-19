81f862568d2303a50c0c2bfcdb3a37474896dc59
rachelbobango <rachelbobango@gmail.com>
2021-04-19 02:14:58 -0400

    peer eval page

--- peer_eval/config/routes.rb
+++ peer_eval/config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 	resources :memberships
 	resources :scores
 	resources :reviews
+	resources :peer_evaluations
 	root to: 'students#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
