d36386b62b4fe4a6f6a97ca3f11b701ce8debff4
noahz <noahzhang1@gmail.com>
2021-04-19 03:19:12 -0400

    Added sign in redirect path

--- peer_eval/app/controllers/application_controller.rb
+++ peer_eval/app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_student!
+
+
+  def after_sign_in_path_for(resource)
+    if current_student.try(:isAdmin?)
+      return "/students"
+    else
+      return "/peer_evaluations"
+    end
+  end
 end
