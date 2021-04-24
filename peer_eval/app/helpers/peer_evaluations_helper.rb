f4fbc40049980168d76e2ab4924d2be117ae1ee1
noahz <noahzhang1@gmail.com>
2021-04-24 15:14:07 -0400

    Fixed duplicated scores

--- peer_eval/app/helpers/peer_evaluations_helper.rb
+++ peer_eval/app/helpers/peer_evaluations_helper.rb
@@ -1,9 +1,9 @@
 module PeerEvaluationsHelper
   def setup_review(review, user, group)
-    puts group.id
-    Membership.where(group_id: group.id).each do |membership|
-      puts membership.student_id
-      review.scores.build(author_id: user.id, recipient_id: membership.student_id)
+    if review.scores.empty?
+      Membership.where(group_id: group.id).each do |membership|
+        review.scores.build(author_id: user.id, recipient_id: membership.student_id)
+      end
     end
     review
   end
